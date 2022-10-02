9595eea6011330fd54164a78cbb1393f613800bb
Abdul Khan <amirkhannitw@gmail.com>
2022-10-02 15:10:55 -0400

    My first git rep

---
+++ Lab2/tablestyle.js
@@ -0,0 +1,7 @@
+document.querySelector("html").style.fontFamily = "sans-serif";
+const table1 = document.getElementsByClassName("mytable");
+table1.style.borderCollapse = "collapse";
+table1.style.border = "2px solid rgb(200,200,200)";
+table1.style.letterSpacing = "1px";
+table1.style.fontSize = "0.8rem";
+table1.style.opacity = "0.5";
